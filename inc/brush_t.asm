b8c21a19a5c68a32ffb9924cef2fc82547e6cdc9
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-03-12 22:01:44 +0000

    %%%%%%%%%%%%%%%

---
+++ inc/brush_t.asm
@@ -0,0 +1,12 @@
+;brush_t
+
+section .brush_t
+ global brush_t
+
+  brush_t:
+
+  Brush_Name db 
+  bnumid dd 0
+
+
+  end brush_t
---
+++ src/brush.asm
@@ -0,0 +1,8 @@
+%include inc/brush_t.asm
+
+g_nBrushId dd 0
+
+;BrushName
+BrushName db:
+ mov , eax, g_nBrushId, ++
+
