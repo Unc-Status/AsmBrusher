12a9cf96911bde10caf13bb3f09e2ece9fe42969
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-03 10:35:19 -0700

    Update brush.asm

--- src/brush.asm
+++ src/brush.asm
@@ -1,21 +1,5 @@
-%include "inc/brush_t.asm"
+;====================
+;    globals
 
 section .bss
-b res 1
-
-end
-
-;brush define mayjor
-%define BRUSH_MAYJOR "Brush"
-
-;=================
-;globals
-
-g_nBrushId dd 0
-m_bBrushPrimitMode db 1 | 0 
-
-BrushName db:
-    cBuff dw '1024'
-    lea rsi, brush_t 
-end BrushName
-
+g_nBrushId DQ 0
